ead05f0945b93999ee739b504820e7ac2328b7fa
Simon Browning <simonbrowning86@gmail.com>
2017-09-12 21:20:10 +0100

    commiting changes so far

--- actions/sendRequest.js
+++ actions/sendRequest.js
@@ -12,54 +12,62 @@ const operation = retry.operation(config.retry);
 //function for sending requests
 const sendRequest = function(method, options, cb) {
 	return new Promise(function(resolve, reject) {
-		let data = '',
-			response;
-		if (!_.has(options, 'path') || !method) {
-			log.info('No path / method was set');
-		}
-		//Options for reqest
-		const _options = {
-			method: method,
-			uri: config.harvest.project_url,
-			headers: {
-				'User-Agent': 'node-harvest',
-				'Content-Type': 'application/json',
-				Accept: 'application/json',
-				Authorization: config.harvest.auth
-			},
-			json: true // Automatically parses the JSON string in the response
-		};
-		//Loop through and merge options
-		for (let key in options) {
-			if (options.hasOwnProperty(key)) {
-				if (key === 'path') {
-					_options.uri += options[key];
-				} else {
-					_options[key] = options[key];
+		operation.attempt(function(currentAttempt) {
+			let response,
+				data = '';
+			if (!_.has(options, 'path') || !method) {
+				log.info('No path / method was set');
+			}
+			//Options for reqest
+			const _options = {
+				method: method,
+				uri: config.harvest.project_url,
+				headers: {
+					'User-Agent': 'node-harvest',
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					Authorization: config.harvest.auth
+				},
+				json: true // Automatically parses the JSON string in the response
+			};
+			//Loop through and merge options
+			for (let key in options) {
+				if (options.hasOwnProperty(key)) {
+					if (key === 'path') {
+						_options.uri += options[key];
+					} else {
+						_options[key] = options[key];
+					}
 				}
 			}
-		}
-		//Make request
-		throttledRequest(_options)
-			//TODO: Add rety method for failed requests
-			.on('response', function(resp) {
-				response = resp;
-				if (/5\d{2}|4\d{2}/.test(response.statusCode)) {
-					if (/5\d{2}/.test(response.statusCode)) {
-					} else {
+			console.log('Attempt operation');
+			//Make request
+
+			throttledRequest(_options)
+				//TODO: Add rety method for failed requests
+				.on('response', function(resp) {
+					response = resp;
+					console.log(`Code: ${response.statusCode}`);
+					console.log(`Attempt: ${currentAttempt}`);
+					if (/201|203/.test(response.statusCode)) {
+						return resolve(response);
+					}
+				})
+				.on('data', function(chunk) {
+					data += chunk;
+				})
+				.on('end', function() {
+					if (/4\d{2}/.test(response.statusCode)) {
 						console.log('reject');
 						return reject(response.statusMessage);
 					}
-				} else if (/201|203/.test(response.statusCode)) {
-					return resolve(response);
-				}
-			})
-			.on('data', function(chunk) {
-				data += chunk;
-			})
-			.on('end', function() {
-				resolve(JSON.parse(data || '{}'));
-			});
+					if (operation.retry(response.statusCode == 503)) {
+						console.log('retry');
+						return;
+					}
+					return resolve(JSON.parse(data || '{}'));
+				});
+		});
 	});
 }; //sendRequest
 
---
+++ tasks/purge_projects.js
@@ -0,0 +1,33 @@
+var r = require('request'),
+	throttledRequest = require('throttled-request')(r),
+	config = require('../config'),
+	sendRequest = require('../actions/sendRequest');
+
+// throttledRequest.on('request', function () {
+//   console.log('Making a request. Elapsed time: %d ms', Date.now() - startedAt);
+// });
+
+function callback(body) {
+	console.log('Got projects now loop');
+	//var projects = JSON.parse(body);
+	body.forEach(function(Obj) {
+		let PID = Obj.project.id;
+		if (PID != config.harvest.default_project) {
+			console.log(`${PID} to be deleted.`);
+			sendRequest('GET', { path: `/projects/${PID}/` })
+				.then(function() {
+					console.log(`${PID} deleted.`);
+				})
+				.catch(function(reason) {
+					console.log(`${PID} failed, ${reason}`);
+				});
+		}
+	});
+}
+console.log(config.harvest.project_url);
+console.log('Get Projects');
+sendRequest('GET', { path: '/projects' })
+	.then(callback)
+	.catch(function(reason) {
+		console.log(`Failed: ${reason}`);
+	});
