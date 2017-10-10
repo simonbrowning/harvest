7aac1c93cf4397f8dad229e2860b571b343a103e
Simon Browning <simonbrowning86@gmail.com>
2017-10-10 14:12:56 +0100

    cron job

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const { execFile } = require('child_process');
-console.log(process.env.NODE_ENV);
+const { execFile } = require('child_process'),
+	CronJob = require('cron').CronJob,
+	slack = require('./actions/slack'),
+	config = require('./config');
 
 execFile('node', [`${__dirname}/services/http.js`], {
 	detached: true,
 	stdio: 'ignore'
 });
 
-//TODO: sort out cronjob
-//cron job for monthly roll over
-// const monthlyRolloverJob = new CronJob(
-// 	'* * * 06 */1 *',
-// 	function() {
-// 		console.log('Monthly rollover triggered');
-// 		execFile('node', [`${__dirname}/services/monthly.js`], {
-// 			detached: true,
-// 			stdio: 'ignore'
-// 		});
-// 	},
-// 	function() {
-// 		/* This function is executed when the job stops */
-// 		console.error('monthlyRolloverJob stopped');
-// 	},
-// 	true /* Start the job right now */
-// );
+console.log('starting');
+const monthlyRolloverJob = new CronJob(
+	'00 00 01 01 */1 *',
+	function() {
+		slack({ channel: config.slack.channel }, 'Monthly rollover is rolling!');
+			function() {
+				execFile('node', [`${__dirname}/services/monthly.js`], {
+					detached: true,
+					stdio: 'ignore'
+				});
+	},
+	function() {
+		/* This function is executed when the job stops */
+		slack(
+			{ channel: config.slack.channel },
+			'Monthly rollover cron job has stopped'
+		);
+	},
+	true /* Start the job right now */
+);
+
+console.log(monthlyRolloverJob);
