b89f667cd78dc9e712ae3b201bfdde02817ebf79
Simon Browning <simonbrowning86@gmail.com>
2017-10-10 12:03:06 +0100

    tasks for project bulk edit

--- tasks/renameProjects.js
+++ tasks/renameProjects.js
@@ -1,7 +1,8 @@
 process.env.NODE_ENV === 'dev';
 
 var config = require('../config'),
-	sendRequest = require('../actions/sendRequest');
+	sendRequest = require('../actions/sendRequest'),
+	moment = require('moment');
 
 function callback(body) {
 	console.log('Got projects now loop');
@@ -10,12 +11,25 @@ function callback(body) {
 		let PID = project.id;
 		if (
 			PID != config.harvest.default_project &&
-			project.name === 'Services - 2017-11'
+			/(.+)\d{4}\-\d{2}$/.test(project.name) &&
+			!!project.active
 		) {
-			console.log(`${PID} to be deleted.`);
-			sendRequest('DELETE', { path: `/projects/${PID}/` })
+			console.log(`${PID} to be renamed.`);
+			sendRequest('PUT', {
+				path: `/projects/${PID}/`,
+				body: {
+					project: {
+						name:
+							project.name.match(/(.+)\d{4}\-\d{2}$/)[1] +
+							moment()
+								.subtract(2, 'months')
+								.format('YYYY-MM'),
+						client_id: project.client_id
+					}
+				}
+			})
 				.then(function() {
-					console.log(`${PID} deleted.`);
+					console.log(`${PID} renamed.`);
 				})
 				.catch(function(reason) {
 					console.log(`${PID} failed, ${reason}`);
