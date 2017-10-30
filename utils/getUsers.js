6ec941a5c0add8833c5ccb262be64e5e9bc3b2bc
Simon Browning <simonbrowning86@gmail.com>
2017-10-30 11:33:29 +0000

    adding deleted files

---
+++ utils/getUsers.js
@@ -0,0 +1,14 @@
+const getPages = require('../actions/getPages.js'),
+log = require('../actions/logging.js');
+
+module.exports = function(data) {
+	return new Promise(async function(resolve, reject) {
+    try {
+     data.users = await getPages('people');
+    } catch (e) {
+      log.warn('Failed to get users '+e);
+      reject(e;
+    }
+    resolve(data);
+	});
+};
