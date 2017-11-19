9ed6887cfd5e982ad4534383fc60ff9b2b9671df
Simon Browning <simonbrowning86@gmail.com>
2017-11-19 14:50:13 +0000

    formatiing update

--- utils/getTasksAssignment.js
+++ utils/getTasksAssignment.js
@@ -3,16 +3,11 @@ const sendRequest = require('../actions/sendRequest.js'),
 
 module.exports = function(data) {
 	return new Promise(function(resolve, reject) {
-		log.info(
-			`${data.old_project.client.name}: ${data.old_project
-				.id} getting old tasks`
-		);
+		log.info(`${data.old_project.client.name}: ${data.old_project.id} getting old tasks`);
 		sendRequest('GET', {
 			path: `/projects/${data.old_project.id}/task_assignments`
 		}).then(function(tasks) {
-			log.info(
-				`${data.old_project.client.name}: ${data.old_project.id} received tasks`
-			);
+			log.info(`${data.old_project.client.name}: ${data.old_project.id} received tasks`);
 			data.tasks = tasks.task_assignments;
 			resolve(data);
 		});
