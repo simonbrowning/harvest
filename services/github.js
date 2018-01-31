e471668f902b22185c17e136134b8709e163299f
Simon Browning <simonbrowning86@gmail.com>
2018-01-31 12:55:03 +0000

    committing webhook for harvest test

---
+++ services/github.js
@@ -0,0 +1,16 @@
+let last_request = {},
+	previous_process;
+
+const fastify = require('fastify')(),
+	_ = require('underscore'),
+	{ exec } = require('child_process';
+
+fastify.post('/github-commit', function(req, res) {
+	exec('cd /home/simon/harvest && git pull');
+	res.send(202, 'Accepted\n');
+});
+
+fastify.listen(3001, function(err) {
+	if (err) throw err;
+	console.log(`server listening on ${fastify.server.address().port}`);
+});
