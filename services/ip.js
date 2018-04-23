46af9abeb7c62788893ca4b41ff4d1693b6f6cb5
Simon Browning <simonbrowning86@gmail.com>
2018-04-23 10:29:29 +0100

    update ip update script

--- services/ip.js
+++ services/ip.js
@@ -3,13 +3,11 @@ const xml = require("xml-js");
 const CronJob = require('cron').CronJob;
 
 
-
+const hosts = "sathu.li,*.sathu.li,harvest.sathu.li";
 let currentIP = "";
 
-const dns = new CronJob(
-	'00 */20 * * * *',
-	function() {
-		request("https://api.ipify.org?format=json", function(error, response, body) {
+function updateIP(){
+  request("https://api.ipify.org?format=json", function(error, response, body) {
   try {
     let ip = JSON.parse(body).ip;
     console.log(`IP: ${ip}`);
@@ -18,18 +16,12 @@ const dns = new CronJob(
     } else {
       currentIP = ip;
       request(
-        `https://zonomi.com/app/dns/dyndns.jsp?action=SET&name=sathu.li&value=${ip}&type=A&api_key=79465433182435492048293076832047955356`,
+        `https://zonomi.com/app/dns/dyndns.jsp?action=SET&name=${hosts}&value=${ip}&api_key=79465433182435492048293076832047955356`,
         function(error, reponse, body) {
-          //console.log(body);
-          //let parsed = xml.parse(body);
           let parsedXML = xml.xml2json(body, { compact: true, spaces: 4 });
           let xmlObj = JSON.parse(parsedXML);
           let updates = xmlObj.dnsapi_result.result_counts._attributes;
-          if(updates.unchanged){
-              console.log(`IP Address unchanged`);
-          }else if(updates.changed){
-            console.log(`IP Address updated`);
-          }
+          console.log(updates);
         }
       );
     }
@@ -37,10 +29,16 @@ const dns = new CronJob(
     console.error(`FAILED: ${e}`);
   }
 });
-	},
+}
+
+const dns = new CronJob(
+	'00 */20 * * * *',
+	function() {
+		updateIP();
+    },
 	function() {/* This function is executed when the job stops */},
 	true /* Start the job right now */
 );
 
 
-
+updateIP();
