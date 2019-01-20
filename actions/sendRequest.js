989c1cabfc684131c631d56cf4c1d75494a945bb
Simon Browning <simonbrowning86@gmail.com>
2019-01-20 22:36:21 +0000

    finally set up caching!

--- actions/sendRequest.js
+++ actions/sendRequest.js
@@ -1,11 +1,13 @@
 const r = require('request'),
 	throttledRequest = require('throttled-request')(r),
+	throttlecache = require('throttled-request')(r),
 	_ = require('underscore'),
 	config = require('../config'),
 	log = require('../actions/logging.js');
 
 //configure request throttle
 throttledRequest.configure(config.throttle);
+throttlecache.configure(config.cache.throttle);
 
 //function for sending requests
 const sendRequest = function(method, options) {
@@ -55,12 +57,12 @@ const sendRequest = function(method, options) {
 					// } else
 					if (response.statusCode == 429) {
 						if (config.retry.maxRetryies > retry) {
-							log.info('Throttled, retrying');
+							log.warn('Throttled, retrying');
 							setTimeout(function() {
 								send(options, cb, ++retry);
 							}, Math.floor(config.retry.timeout * (Math.random() * 10)));
 						} else {
-							log.info('Giving up');
+							log.warn('Giving up');
 							return cb('failed', data || response.statusMessage);
 						}
 					} else if (/4\d{2}/.test(response.statusCode)) {
@@ -68,19 +70,71 @@ const sendRequest = function(method, options) {
 						return cb('failed', error.error || error.error_description || data);
 					}
 					if (/2\d{2}/.test(response.statusCode)) {
-						return cb('success', JSON.parse(data || '{}'));
+						if (response.request.method == "GET") {
+							let name = options.uri.substr(29);
+							r(
+                                {
+									uri: config.cache.url,
+									method: "POST",
+                                    form: {
+                                        name: name,
+                                        value: data
+                                    }
+                                },
+                                function(err, resp, body) {
+                                    return cb(
+                                        "success",
+                                        JSON.parse(
+                                            data || "{}"
+                                        )
+                                    );
+                                }
+                            );
+						} else {
+							return cb("success", JSON.parse(data || "{}"));
+						}
+						
 					} else {
 						return cb('failed', data);
 					}
 				});
 		};
-		send(
-			_options,
-			function(status, response) {
-				return status === 'success' ? resolve(response) : reject(response);
-			},
-			0
-		);
+
+		if (method === "GET") {
+			r.get(
+				`${config.cache.url}?key=${options.path}`,
+				function (err, resp, body) {
+					let data;
+					if (body) {
+						try {
+							data = JSON.parse(body);
+						} catch (e) {
+							log.warn(`failed to parse ${body}`);
+						}
+						if (data) {
+							return resolve(data);
+						}
+					} else {
+						send(
+							_options,
+							function (status, response) {
+								return status === 'success' ? resolve(response) : reject(response);
+							},
+							0
+						);
+					}
+				}
+			);
+		} else { 
+			send(
+				_options,
+				function (status, response) {
+					return status === 'success' ? resolve(response) : reject(response);
+				},
+				0
+			);
+		}
+		
 	});
 }; //sendRequest
 
---
+++ services/cache.js
@@ -0,0 +1,67 @@
+process.env.log = 'cache';
+
+let last_request = {},
+    previous_process;
+
+const express = require('express'),
+    _ = require('underscore'),
+    { execFile } = require('child_process'),
+    log = require('../actions/logging.js'),
+    bodyParser = require("body-parser"),
+    NodeCache = require("node-cache");
+const myCache = new NodeCache({ stdTTL: 72000 });
+const app = express();
+
+//Here we are configuring express to use body-parser as middle-ware.
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+
+
+app.delete('/cache', function (req, res) {
+    myCache.flushAll();
+    res.type("json");
+    return res.send(JSON.stringify({ message: "cache flushed" }));
+});
+app.get("/cache/stats", function(req, res) {
+    res.type("json");
+    return res.send(JSON.stringify(myCache.stats));
+});
+
+app.get('/cache', function (req, res) {
+    let key = decodeURI(req.query.key);
+    console.log(`New request for ${key}`);
+    res.type("json");
+
+    if (!key) {
+        return res.send(JSON.stringify({ 'message': "no key provided" }));
+    }
+
+    myCache.get(key, function (err, value) { 
+        if (err) {
+            console.log(`Error ${err}`);
+            return res.send(JSON.stringify({ 'message': err }));  
+        }
+        console.log(`${value ?  'SENDING CACHE' : 'NO CACHED VALUE'}`);
+        return res.send(value? JSON.stringify(value) : null);
+    });
+})
+
+app.post('/cache', function (req, res) {
+    let new_entry = req.body;
+    res.type("json");
+    let data = JSON.parse(new_entry.value);
+    console.log(`Set entry ${new_entry.name}`);
+    myCache.set(new_entry.name, data, function (err,success) { 
+        if (err) { 
+            return res.send(JSON.stringify({ 'message': err }));
+        }
+        return res.send(JSON.stringify({ 'message': success ? "Cahce Set": "Cache Not Set" }));
+    })
+});
+
+app.listen(3002, function (err) {
+    if (err) throw err;
+    log.info(`Express server listening on 3000`);
+});
