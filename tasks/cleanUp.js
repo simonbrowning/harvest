36d2b0e580b7d2ec9ef23c84d079a4e8ea295254
Simon Browning <simonbrowning86@gmail.com>
2019-02-01 13:53:01 +0000

    update cleanup to clearcache when finished

--- tasks/cleanUp.js
+++ tasks/cleanUp.js
@@ -1,8 +1,12 @@
-const cleanUp = require('../actions/checkupdate');
+const cleanUp = require('../actions/checkupdate'),
+	request = require('request');
 
 async function start() {
 	console.log('starting');
 	await cleanUp();
+	request.delete('http://127.0.0.1:3002/cache', function () {
+		console.log('New Services project created clearing cache');
+	});
 	console.log('finished');
 }
 
