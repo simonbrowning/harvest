fc7ef2b96bd2275b9389cea8623be1841c10d5bc
Smon Browning <simonbrowning86@gmail.com>
2019-11-01 14:58:25 +0000

    upped cache limit

--- services/cache.js
+++ services/cache.js
@@ -14,8 +14,8 @@ const app = express();
 
 //Here we are configuring express to use body-parser as middle-ware.
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 
 
 
